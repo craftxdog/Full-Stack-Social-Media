0ccaf43ab88045432219d8fe71b1303a7fd7480b
Mr. Ulloa Silva <aaronulloa.tcp@gmail.com>
2023-12-08 18:58:13 -0600

    Library Update 2

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -60,15 +60,14 @@ export const useGetPosts = () => {
         queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
         queryFn: getInfinitePosts as any,
         getNextPageParam: (lastPage: any) => {
-            // If there's no data, there are no more pages.
             if (lastPage && lastPage.documents.length === 0) {
                 return null;
             }
 
-            // Use the $id of the last document as the cursor.
             const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
             return lastId;
         },
+        initialPageParam: undefined, // Agrega esta línea
     });
 };
 
